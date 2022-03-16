6928cbdb9ecfa726099be303c235b2f18233fd6c
ArtemVasilev16 <92008546+ArtemVasilev16@users.noreply.github.com>
2022-03-16 15:52:52 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import tabsFunc from './modules/tabs'
+import accordionFunc from './modules/accordion'
+import burgerFunc from './modules/burger'
+import modalFunc from './modules/modal'
+import sedndFormFunc from './modules/sedndForm'
+import smoothScrollFunc from './modules/smoothScroll'
+
+tabsFunc()
+accordionFunc()
+burgerFunc()
+modalFunc()
+sedndFormFunc('.form-test-drive')
+smoothScrollFunc()
